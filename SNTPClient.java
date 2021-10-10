120c84f94a13fa94dd8e5aa598dbf7978643d22b
Aslam Anver <slam.anver9@gmail.com>
2021-10-10 12:35:53 +0530

    Update SNTPClient.java

--- SNTPClient.java
+++ SNTPClient.java
@@ -2,6 +2,8 @@
  * Original work Copyright (C) 2008 The Android Open Source Project
  * Modified work Copyright (C) 2019, Aslam Anver
  *
+ * https://github.com/aslamanver/sntp-client-android
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
